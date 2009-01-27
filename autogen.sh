10cfde2ffb9020fa566a3135acb58ad2d0a850f0
Andreas Kemnade <andreas@kemnade.info>
2009-01-27 17:32:00 +0100

    use m4 directory

--- autogen.sh
+++ autogen.sh
@@ -81,48 +81,33 @@ case $CC in
 xlc )
   am_opt=--include-deps;;
 esac
-
-for coin in `find $srcdir -name configure.in -print`
-do 
-  dr=`dirname $coin`
-  if test -f $dr/NO-AUTO-GEN; then
-    echo skipping $dr -- flagged as no auto-gen
+  dr=$srcdir
+  macrodirs=m4
+  aclocalinclude="-I m4"
+  if grep "^AM_GNU_GETTEXT" configure.in >/dev/null; then
+  if grep "sed.*POTFILES" configure.in >/dev/null; then
+  : do nothing -- we still have an old unmodified configure.in
   else
-    echo processing $dr
-    macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
-    ( cd $dr
-      aclocalinclude="$ACLOCAL_FLAGS"
-      for k in $macrodirs; do
-  	if test -d $k; then
-          aclocalinclude="$aclocalinclude -I $k"
-  	##else 
-	##  echo "**Warning**: No such directory \`$k'.  Ignored."
-        fi
-      done
-      if grep "^AM_GNU_GETTEXT" configure.in >/dev/null; then
-	if grep "sed.*POTFILES" configure.in >/dev/null; then
-	  : do nothing -- we still have an old unmodified configure.in
-	else
-	  echo "Creating $dr/aclocal.m4 ..."
-	  test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
-	  echo "Running gettextize...  Ignore non-fatal messages."
+  echo "Creating $dr/aclocal.m4 ..."
+  test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
+  echo "Running gettextize...  Ignore non-fatal messages."
 #	  echo "no" | gettextize --force --copy --intl
-	  echo "Making $dr/aclocal.m4 writable ..."
-	  test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
-        fi
-      fi
-      if grep "^AM_GNOME_GETTEXT" configure.in >/dev/null; then
-	echo "Creating $dr/aclocal.m4 ..."
-	test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
-	echo "Running gettextize...  Ignore non-fatal messages."
+  echo "Making $dr/aclocal.m4 writable ..."
+  test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
+  fi
+  fi
+  if grep "^AM_GNOME_GETTEXT" configure.in >/dev/null; then
+echo "Creating $dr/aclocal.m4 ..."
+test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
+echo "Running gettextize...  Ignore non-fatal messages."
 #	echo "no" | gettextize --force --copy
-	echo "Making $dr/aclocal.m4 writable ..."
-	test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
-      fi
-      if grep "^AM_PROG_LIBTOOL" configure.in >/dev/null; then
+echo "Making $dr/aclocal.m4 writable ..."
+test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
+  fi
+  if grep "^AM_PROG_LIBTOOL" configure.in >/dev/null; then
 	echo "Running libtoolize..."
 	libtoolize --force --copy
-      fi
+  fi
       echo "Running aclocal $aclocalinclude ..."
       aclocal $aclocalinclude
       if grep "^AM_CONFIG_HEADER" configure.in >/dev/null; then
@@ -133,9 +118,7 @@ do
       automake --add-missing --gnu $am_opt
       echo "Running autoconf ..."
       autoconf
-    )
-  fi
-done
+    
 
 #conf_flags="--enable-maintainer-mode --enable-compile-warnings" #--enable-iso-c
 
